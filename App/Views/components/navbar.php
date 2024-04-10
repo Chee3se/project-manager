b36972cab80b82271efd0023353af94334ba4286
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-10 14:01:42 +0300

    styling changes

--- App/Views/components/navbar.php
+++ App/Views/components/navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <a class=<?php if ($_SERVER['REQUEST_URI'] == "/") {echo "active";} else {echo "none";}?> href="/">Home</a>
-    <!-- Lai aizvietotu pārējo -->
-    <div></div>
+    <a class=<?php if ($_SERVER['REQUEST_URI'] == "/tasks") {echo "active";} else {echo "none";}?> href="/tasks">My Tasks</a>
+    <a class="login <?php if ($_SERVER['REQUEST_URI'] == "/login") {echo "active";} else {echo "none";}?>" href="/login">Login</a>
 </nav>
 <main>
--- App/Views/home/index.view.php
+++ App/Views/home/index.view.php
@@ -1,4 +1,4 @@
 <?php component('header', compact('page_title')); ?>
 <?php component('navbar'); ?>
-    <h1>Welcome!</h1>
+    <h1 class='index-h1'>Welcome to the best <br> project management website!</h1>
 <?php component('footer'); ?>
