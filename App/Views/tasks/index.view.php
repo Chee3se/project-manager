6a0cec64db4325647f2ffce44dabc55c291295c6
eaterz <muraskoruslans@gmail.com>
2024-04-29 12:38:14 +0300

    delete button for tasks

--- App/Views/tasks/index.view.php
+++ App/Views/tasks/index.view.php
@@ -9,7 +9,6 @@
             <h2>To do</h2>
         <?php foreach ($tasks as $task): ?>
         <?php if($task['status']=='assigned'){?>
-
                 <a href="/tasks/edit?id=<?=$task['id']?>" draggable="true" ondragstart="drag(event)" id="<?= $task['id'] ?>">
                     <div class="task" >
                         <form class="delete_form" method="post" action="/tasks">
@@ -39,6 +38,15 @@
             <?php if($task['status']=='pending'){?>
                 <a href="/tasks/edit?id=<?=$task['id']?>"  draggable="true" ondragstart="drag(event)" id="<?= $task['id'] ?>">
                     <div class="task">
+                        <form class="delete_form" method="post" action="/tasks">
+                            <input type="hidden" name="id" value="<?= $task['id']?>" />
+                            <input type="hidden" name="_method" value="DELETE" />
+                            <div class="delete">
+                                <div>
+                                    <button >X</button>
+                                </div>
+                            </div>
+                        </form>
                         <p><?= $task['description'] ?></p>
                         <p><?= $task['start_date']?></p>
                         <p><?= $task['due_date'] ?></p>
@@ -48,12 +56,22 @@
         <?php endforeach; ?>
     </div>
 
+
     <div class="task_type" ondrop="drop(event, 'completed')" ondragover="allowDrop(event)">
             <h2>Done</h2>
         <?php foreach ($tasks as $task): ?>
             <?php if($task['status']=='completed'){?>
                 <a href="/tasks/edit?id=<?=$task['id']?>" draggable="true" ondragstart="drag(event)" id="<?= $task['id'] ?>">
                     <div class="task" >
+                        <form class="delete_form" method="post" action="/tasks">
+                            <input type="hidden" name="id" value="<?= $task['id']?>" />
+                            <input type="hidden" name="_method" value="DELETE" />
+                            <div class="delete">
+                                <div>
+                                    <button >X</button>
+                                </div>
+                            </div>
+                        </form>
                         <p><?= $task['description'] ?></p>
                         <p><?= $task['start_date']?></p>
                         <p><?= $task['due_date'] ?></p>
